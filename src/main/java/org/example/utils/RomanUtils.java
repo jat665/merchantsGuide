404fb85e4bf4db666259fc43bb48ca9565299cf6
Jorge Arindia <jorgearindia@gmail.com>
2023-04-28 03:18:41 -0500

    test single roman numerals

--- src/main/java/org/example/utils/RomanUtils.java
+++ src/main/java/org/example/utils/RomanUtils.java
@@ -1,8 +1,24 @@
 package org.example.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanUtils {
 
+    Map<Character, Integer> romanMap = new HashMap<>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
     public int toNumber(String roman) {
-        return 1;
+        if (!romanMap.containsKey(roman.charAt(0))) {
+            return -1;
+        }
+        return romanMap.get(roman.charAt(0));
     }
 }
--- src/test/java/utils/RomanUtilsTest.java
+++ src/test/java/utils/RomanUtilsTest.java
@@ -17,6 +17,42 @@ public class RomanUtilsTest {
     @Test
     public void testToNumber1() {
         int result = romanUtils.toNumber("I");
-        assertEquals("'I' should be equal 1", 1, result);
+        assertEquals("should be equal to 1", 1, result);
+    }
+
+    @Test
+    public void testToNumber5() {
+        int result = romanUtils.toNumber("V");
+        assertEquals("should be equal to 5", 5, result);
+    }
+
+    @Test
+    public void testToNumber10() {
+        int result = romanUtils.toNumber("X");
+        assertEquals("should be equal to 10", 10, result);
+    }
+
+    @Test
+    public void testToNumber50() {
+        int result = romanUtils.toNumber("L");
+        assertEquals("should be equal to 50", 50, result);
+    }
+
+    @Test
+    public void testToNumber100() {
+        int result = romanUtils.toNumber("C");
+        assertEquals("'C' should be equal to 100", 100, result);
+    }
+
+    @Test
+    public void testToNumber500() {
+        int result = romanUtils.toNumber("D");
+        assertEquals("'C' should be equal to 500", 500, result);
+    }
+
+    @Test
+    public void testToNumber1000() {
+        int result = romanUtils.toNumber("M");
+        assertEquals("'C' should be equal to 1000", 1000, result);
     }
 }
